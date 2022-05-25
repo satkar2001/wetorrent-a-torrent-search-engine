eeb2b134c2f176c39260317bf5d8d9eb0443c900
satkar2001 <satkaracharya883@gmail.com>
2022-05-25 11:54:45 +0530

    .

---
+++ backend/torrent.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose") ;
+
+const schema=new mongoose.Schema(
+    {
+        Seeders:Number,
+        Leechers:Number,
+        Source:{
+            type:String,
+            trim:true
+        },
+        Link:{
+            type:String,
+            trim:true
+        },
+        _id:{
+            type:String,
+            trim:true
+        },
+        Size:Number,
+        UploadDate:Number 
+
+    },{versionKey:false}
+);
+
+module.exports.torrent=mongoose.model("torrents",schema);
